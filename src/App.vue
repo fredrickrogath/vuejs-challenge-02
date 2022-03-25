2e44d9c66bdada0417f66928052a57e1950d47be
fredrick rogath <fredlee560@gmail.com>
2022-03-25 22:17:47 +0300

    vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<the-header title="frs"></the-header>
+<the-resource></the-resource>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResource from './components/learning-resourses/TheResource.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    TheHeader,
+    TheResource,
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/learning-resourses/TheResource.vue
@@ -0,0 +1,60 @@
+<template>
+  <base-card>
+    <base-button :mode="storedResSelected" @click="setSelectedTab('stored-resourses')"
+      >Stored resources</base-button
+    >
+    <base-button :mode="addResSelected" @click="setSelectedTab('add-resource')"
+      >Add Resource</base-button
+    >
+  </base-card>
+
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResourses from "./StoredResourses.vue";
+import AddResource from "./AddResource.vue";
+export default {
+  components: {
+    StoredResourses,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: "stored-resourses",
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vuejs documentation",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google...",
+          link: "https://google.com",
+        },
+      ],
+    };
+  },
+  provide(){
+      return {
+          resources: this.storedResources,
+      }
+  },
+  computed:{
+    storedResSelected(){
+      return this.selectedTab !== 'stored-resourses'? 'flat':null;
+    },
+    addResSelected(){
+      return this.selectedTab !== 'add-resource'? 'flat':null;
+    }
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
